39b5db20634d1bcc4b461d66f9df9e59635923ba
hoomili <hasadian@hotmail.com>
2023-06-01 23:49:08 -0700

    oreder page is fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = LineItem.where(order_id: params[:id])
+    @products = Product.all
+    
   end
 
   def create
