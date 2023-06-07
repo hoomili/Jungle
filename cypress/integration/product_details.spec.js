71bdd20c4ac859ec5a3850d1919b523d73e04bb0
hoomili <hasadian@hotmail.com>
2023-06-07 13:07:21 -0700

    Feature Spec: Product Details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,12 @@
+describe('Jungle app', () => {
+  beforeEach("visit the main page", () => {
+    cy.visit("http://localhost:3000/")
+    cy.get(".form-control").first().type("tipsy@a.com")
+    cy.get(".form-control").last().type("123456")
+    cy.get(".btn-primary").click()
+  })
+  it("There is 2 products on the page", () => {
+    cy.get("article").first().click();
+  });
+
+})
